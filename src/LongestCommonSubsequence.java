bc89afd0758842684af67d802ff61cece2591e32
Ryo <shiyanch@gmail.com>
2016-10-18 22:26:30 -0400

    Dynamic Programming/Longest Common Subsequence

--- src/LongestCommonSubsequence.java
+++ src/LongestCommonSubsequence.java
@@ -24,23 +24,19 @@ public class LongestCommonSubsequence {
         int m = s1.length();
         int n = s2.length();
 
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[m+1][n+1];
 
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(i==0 && j==0)
-                    dp[i][0] = s1.charAt(i) == s2.charAt(0) ? 1 : 0;
-                else if(i==0)
-                    dp[i][j] = s1.charAt(i) == s2.charAt(j)?1:0 + dp[i][j-1];
-                else if(j==0)
-                    dp[i][j] = s1.charAt(i) == s2.charAt(j)?1:0 + dp[i-1][j];
-                else if(s1.charAt(i) == s2.charAt(j))
+        for(int i=0;i<=m;i++) {
+            for(int j=0;j<=n;j++) {
+                if(i==0 || j==0)
+                    dp[i][j] = 0;
+                else if(s1.charAt(i-1) == s2.charAt(j-1))
                     dp[i][j] = dp[i-1][j-1]+1;
                 else
                     dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
 }
